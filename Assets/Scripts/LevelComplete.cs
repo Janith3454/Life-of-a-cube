427f7aea2fc641196337117906e3e16ba64adaf6
Janith3454 <44985944+Janith3454@users.noreply.github.com>
2018-11-12 17:21:26 -0600

    Initial Commit

---
+++ Assets/Scripts/LevelComplete.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+
+public class LevelComplete : MonoBehaviour
+{
+    public void loadNextLevel ()
+    {
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+    }
+
+
+
+}
---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,16 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+
+    public Transform player;
+    public Text scoreboard;
+    void Update()
+    {
+
+        scoreboard.text = player.position.z.ToString("0");
+
+    }
+}
